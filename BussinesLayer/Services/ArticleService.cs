69dc2e107eeb03173df077a83735fa75156d4100
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-01-14 18:53:15 -0300

    add form for list Articles

---
+++ BussinesLayer/Services/ArticleService.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Services
+{
+    public class ArticleService
+    {
+        private ArticleRepository articleRepository = new ArticleRepository();
+
+        public DataTable GetArticles()
+        {
+            DataTable dt = new DataTable();
+            dt = articleRepository.GetArticles();
+            return dt;
+        }
+    }
+}
--- PresentacionLayer/Form1.cs
+++ PresentacionLayer/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BussinesLayer.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,17 @@ namespace PresentationLayer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowArticles();
+        }
+
+        private void ShowArticles()
+        {
+            ArticleService objeto = new ArticleService();
+            dataGridView1.DataSource = objeto.GetArticles();
         }
     }
 }
