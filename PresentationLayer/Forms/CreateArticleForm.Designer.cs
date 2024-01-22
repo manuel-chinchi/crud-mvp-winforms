c91e72d7ddfc598533a0039753e06bf4996ddcc3
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-01-22 11:28:00 -0300

    set center form

--- PresentationLayer/Forms/CreateArticleForm.Designer.cs
+++ PresentationLayer/Forms/CreateArticleForm.Designer.cs
@@ -52,6 +52,7 @@ namespace PresentacionLayer.Forms
             this.MinimizeBox = false;
             this.Name = "CreateArticleForm";
             this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CreateArticle";
             this.ResumeLayout(false);
 
