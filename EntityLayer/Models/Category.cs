d0a822914bc13f5fce77a3780d86ad000233c8a4
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-01-27 18:08:29 -0300

    hidden property of Category model on load DataGridView

--- EntityLayer/Models/Category.cs
+++ EntityLayer/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace EntityLayer.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [Browsable(false)]
         public DateTime DateCreated { get; set; }
     }
 }
