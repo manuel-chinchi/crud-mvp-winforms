4ef2cf2c4189a78b38ba993bc17d7a0007702747
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-02-02 21:10:40 -0300

    update report form

--- PresentationLayer/Views/ReportView.Designer.cs
+++ PresentationLayer/Views/ReportView.Designer.cs
@@ -30,32 +30,21 @@ namespace PresentationLayer.Views
         private void InitializeComponent()
         {
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.btnPrint = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
-            this.reportViewer1.Location = new System.Drawing.Point(3, 3);
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(494, 634);
+            this.reportViewer1.Size = new System.Drawing.Size(500, 700);
             this.reportViewer1.TabIndex = 0;
-            // 
-            // btnPrint
-            // 
-            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPrint.Location = new System.Drawing.Point(3, 643);
-            this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(494, 54);
-            this.btnPrint.TabIndex = 1;
-            this.btnPrint.Text = "Print";
-            this.btnPrint.UseVisualStyleBackColor = true;
             // 
             // ReportView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.reportViewer1);
             this.Name = "ReportView";
             this.Size = new System.Drawing.Size(500, 700);
@@ -66,6 +55,5 @@ namespace PresentationLayer.Views
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
-        private System.Windows.Forms.Button btnPrint;
     }
 }
