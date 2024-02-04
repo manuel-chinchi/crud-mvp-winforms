15e6750d115e84f0d6d1e41402b813c0f27777c6
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-02-03 21:45:49 -0300

    add Category list report

--- PresentationLayer/Views/ReportView.cs
+++ PresentationLayer/Views/ReportView.cs
@@ -16,6 +16,7 @@ namespace PresentationLayer.Views
     public partial class ReportView : UserControl
     {
         IArticleService<IEnumerable<Article>> articleService { get; set; } = new ArticleService();
+        ICategoryService<IEnumerable<Category>> categoryService { get; set; } = new CategoryService();
 
         public ReportView()
         {
@@ -40,6 +41,22 @@ namespace PresentationLayer.Views
             //reportViewer1.LocalReport.DataSources.Add(rds);
             //reportViewer1.LocalReport.SetParameters(parms);
             //reportViewer1.RefreshReport();
+
+            //if (rvReport.LocalReport.ReportPath==null)
+            if (1 == 2)
+            {
+                var rootPath = AppDomain.CurrentDomain.BaseDirectory;
+                //rvReport.LocalReport.ReportPath = $"PresentationLayer/Reports/CategoriesReport.rdlc";
+                rvReport.LocalReport.ReportEmbeddedResource = "PresentationLayer.Reports.CategoriesReport.rdlc";
+                var categories = categoryService.GetCategories();
+                rvReport.SetDisplayMode(DisplayMode.PrintLayout);
+                
+                rvReport.LocalReport.DataSources.Clear();
+
+                rvReport.LocalReport.DataSources.Add(new ReportDataSource("dsCategories", categories));
+                rvReport.RefreshReport();
+                return;
+            }
 
             //REF: https://stackoverflow.com/questions/37276185/how-to-get-the-table-at-the-center-of-reportviewer-in-my-form
             rvReport.SetDisplayMode(DisplayMode.PrintLayout);
