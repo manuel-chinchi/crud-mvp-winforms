0de2caab36ed47a3d2fed9d1d2ea68bd4eb6e241
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-02-21 14:39:10 -0300

    add custom code for Reports

---
+++ PresentationLayer/Reports/CustomCode/Functions.vb
@@ -0,0 +1,8 @@
+﻿Public Function SortList(ByVal items As Object()) As Object()
+    If items Is Nothing OrElse items.Length <= 1 Then
+        Return items
+    End If
+
+    Array.Sort(items)
+    Return items
+End Function
