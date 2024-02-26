1fdd333554e2baca24d256d54e0a6995f771f7fa
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-02-25 23:33:01 -0300

    delete static data of form

--- PresentationLayer/Forms/ListCategoriesForm.Designer.cs
+++ PresentationLayer/Forms/ListCategoriesForm.Designer.cs
@@ -29,36 +29,11 @@ namespace PresentationLayer.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            EntityLayer.Models.Category category1 = new EntityLayer.Models.Category();
-            EntityLayer.Models.Category category2 = new EntityLayer.Models.Category();
-            EntityLayer.Models.Category category3 = new EntityLayer.Models.Category();
-            EntityLayer.Models.Category category4 = new EntityLayer.Models.Category();
-            EntityLayer.Models.Category category5 = new EntityLayer.Models.Category();
             this.listCategoriesView1 = new PresentationLayer.Views.ListCategoriesView();
             this.SuspendLayout();
             // 
             // listCategoriesView1
             // 
-            category1.DateCreated = new System.DateTime(2024, 1, 17, 0, 0, 0, 0);
-            category1.Id = 1;
-            category1.Name = "Otro";
-            category2.DateCreated = new System.DateTime(2024, 1, 17, 0, 0, 0, 0);
-            category2.Id = 2;
-            category2.Name = "Pantalones";
-            category3.DateCreated = new System.DateTime(2024, 1, 17, 0, 0, 0, 0);
-            category3.Id = 3;
-            category3.Name = "Camisas";
-            category4.DateCreated = new System.DateTime(2024, 1, 25, 21, 27, 14, 277);
-            category4.Id = 5;
-            category4.Name = "xx";
-            category5.DateCreated = new System.DateTime(2024, 1, 25, 21, 34, 8, 117);
-            category5.Id = 7;
-            category5.Name = "zz";
-            new EntityLayer.Models.SortableBindingList<EntityLayer.Models.Category>().Add(category1);
-            new EntityLayer.Models.SortableBindingList<EntityLayer.Models.Category>().Add(category2);
-            new EntityLayer.Models.SortableBindingList<EntityLayer.Models.Category>().Add(category3);
-            new EntityLayer.Models.SortableBindingList<EntityLayer.Models.Category>().Add(category4);
-            new EntityLayer.Models.SortableBindingList<EntityLayer.Models.Category>().Add(category5);
             this.listCategoriesView1.Location = new System.Drawing.Point(0, 0);
             this.listCategoriesView1.Error = null;
             this.listCategoriesView1.Success = null;
