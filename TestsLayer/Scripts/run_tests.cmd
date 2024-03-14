dc6993ae98e8e6ebc2941e68e9303decfbc4d4cb
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-03-14 19:08:17 -0300

    remove unused files

--- TestsLayer/Scripts/run_tests.cmd
+++
@@ -1,6 +0,0 @@
-@echo off 
-
-REM Este script ejecuta las pruebas del proyecto 'TestsLayer' y crea el archivo de resultados en 
-REM TestsLayer\Results\*.trx
-
-vstest.console.exe %cd%\TestsLayer\bin\Debug\TestsLayer.dll --logger:trx --resultsdirectory:"%cd%\TestsLayer\Results"
