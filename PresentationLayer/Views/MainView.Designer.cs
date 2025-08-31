966ae267ff59a1fc72483beaeebf9c379834427a
Manuel Chinchi <manuel.chinchi93@gmail.com>
2025-08-31 14:32:28 -0300

    [PresentationLayer]: update design on MainView

--- PresentationLayer/Views/MainView.Designer.cs
+++ PresentationLayer/Views/MainView.Designer.cs
@@ -39,12 +39,12 @@ namespace PresentationLayer.Views
             // lblTitle
             // 
             this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblTitle.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(3, 3);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(210, 50);
+            this.lblTitle.Size = new System.Drawing.Size(204, 50);
             this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "MENU";
+            this.lblTitle.Text = "MAIN MENU";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnArticles
@@ -52,9 +52,9 @@ namespace PresentationLayer.Views
             this.btnArticles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnArticles.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnArticles.Location = new System.Drawing.Point(0, 53);
+            this.btnArticles.Location = new System.Drawing.Point(3, 56);
             this.btnArticles.Name = "btnArticles";
-            this.btnArticles.Size = new System.Drawing.Size(210, 50);
+            this.btnArticles.Size = new System.Drawing.Size(204, 50);
             this.btnArticles.TabIndex = 1;
             this.btnArticles.Text = "Articles";
             this.btnArticles.UseVisualStyleBackColor = true;
@@ -64,9 +64,9 @@ namespace PresentationLayer.Views
             this.btnCategories.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCategories.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCategories.Location = new System.Drawing.Point(0, 109);
+            this.btnCategories.Location = new System.Drawing.Point(3, 112);
             this.btnCategories.Name = "btnCategories";
-            this.btnCategories.Size = new System.Drawing.Size(210, 50);
+            this.btnCategories.Size = new System.Drawing.Size(204, 50);
             this.btnCategories.TabIndex = 2;
             this.btnCategories.Text = "Categories";
             this.btnCategories.UseVisualStyleBackColor = true;
@@ -76,9 +76,9 @@ namespace PresentationLayer.Views
             this.btnReports.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnReports.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReports.Location = new System.Drawing.Point(0, 165);
+            this.btnReports.Location = new System.Drawing.Point(3, 168);
             this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(210, 50);
+            this.btnReports.Size = new System.Drawing.Size(204, 50);
             this.btnReports.TabIndex = 3;
             this.btnReports.Text = "Reports";
             this.btnReports.UseVisualStyleBackColor = true;
@@ -88,7 +88,7 @@ namespace PresentationLayer.Views
             this.llbAbout.AutoSize = true;
             this.llbAbout.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.llbAbout.LinkColor = System.Drawing.Color.DodgerBlue;
-            this.llbAbout.Location = new System.Drawing.Point(0, 227);
+            this.llbAbout.Location = new System.Drawing.Point(3, 230);
             this.llbAbout.Name = "llbAbout";
             this.llbAbout.Size = new System.Drawing.Size(45, 17);
             this.llbAbout.TabIndex = 4;
@@ -108,6 +108,7 @@ namespace PresentationLayer.Views
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainView";
+            this.Padding = new System.Windows.Forms.Padding(3);
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MenuView";
